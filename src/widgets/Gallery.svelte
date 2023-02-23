b0d52b188828f4dfe388b2d7b3cb62a52cfd95eb
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-23 13:42:00 +0000

    fix

--- src/widgets/Gallery.svelte
+++ src/widgets/Gallery.svelte
@@ -187,14 +187,14 @@ function imageExists(image_url){
 
 <div class="btn-group float-end">
 
-<button class="btn btn-outline-secondary" on:click="{() => moveDown(key, i)}">
+<button class="btn btn-outline-secondary btn-icon" on:click="{() => moveDown(key, i)}">
 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M3.204 5L8 10.481 12.796 5H3.204zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
 </svg>
 </button>
 
 
-  <button class="btn btn-outline-secondary" on:click="{() => deleteImage(key, i)}">
+  <button class="btn btn-outline-secondary btn-icon" on:click="{() => deleteImage(key, i)}">
   <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -222,7 +222,11 @@ function imageExists(image_url){
 
 .btn {
     margin-bottom: 10px;
-   margin-bottom: 0 !important;
+  
+}
+
+.btn-icon{
+  margin-bottom: 0 !important;
 }
 
 .row .btn{
