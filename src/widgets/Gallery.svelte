cf8ff395f0c2ab312f48d9f6fa147b7a1fc813a5
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-23 13:49:34 +0000

    fix

--- src/widgets/Gallery.svelte
+++ src/widgets/Gallery.svelte
@@ -239,8 +239,8 @@ function imageExists(image_url){
 
 .box{
   display: inline-block;
-  width: 39px;
-  height: 39px;
+  width: 38px;
+  height: 38px;
   border: 1px solid #6c757d;
   background-size: cover;
   border-radius: 4px;
